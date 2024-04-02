0fe32347c994f46359a332e150cdc8435688e2ad
Beulah Kruger <111886142+beulahkr@users.noreply.github.com>
2024-04-02 10:48:38 +0200

    Added state manager for project lists

--- project-manager/src/app.ts
+++ project-manager/src/app.ts
@@ -1,5 +1,34 @@
-//Validation
+// Project State Management
 
+class ProjectState {
+    private projects: any[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {
+
+    }
+
+    static getInstance() {
+        if (this.instance){
+            return this.instance;
+        }
+        this.instance = new ProjectState();
+        return this.instance;
+    }
+    addProject(title: string, description: string, numOfPeople: number){
+        const newProject = {
+            id: Math.random().toString(),
+            title: title,
+            description: description,
+            people: numOfPeople
+        };
+        this.projects.push(newProject);
+    }
+}
+
+const projectState = ProjectState.getInstance();
+
+//Validation
 interface Validatable{
     value: string | number;
     required?: boolean;
@@ -163,7 +192,7 @@ class ProjectInput {
         const userInput = this.gatherUserInput();
         if(Array.isArray(userInput)){
             const [title, description, people] = userInput;
-            console.log(title, description, people);
+            projectState.addProject(title, description, people);
             this.clearInputs();
         }
     }
