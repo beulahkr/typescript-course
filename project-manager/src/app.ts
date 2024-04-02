45dbcf0c3cbf70e14f45ab5ddff819da71482d04
Beulah Kruger <111886142+beulahkr@users.noreply.github.com>
2024-04-02 14:26:40 +0200

    added private method to render project list items to dom

--- project-manager/src/app.ts
+++ project-manager/src/app.ts
@@ -93,13 +93,14 @@ class ProjectList{
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLElement;
-    assignedProjects: any[] = [];
+    assignedProjects: any[];
 
     constructor(private type: 'active' | 'finished') {
         this.templateElement = document.getElementById(
             'project-list'
             )! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        this.assignedProjects = [];
 
         const importedNode = document.importNode(
             this.templateElement.content, 
@@ -110,11 +111,22 @@ class ProjectList{
 
             projectState.addListener((projects: any[]) => {
                 this.assignedProjects = projects;
+                this.renderProjects();
             });
 
         this.attach();
         this.renderContent();
     }
+
+    private renderProjects(){
+        const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        for (const prjItem of this.assignedProjects){
+            const listItem = document.createElement('li');
+            listItem.textContent = prjItem.title;
+            listEl.appendChild(listItem);
+        }
+    }
+
     private renderContent() {
         const listId = `${this.type}-projects-list`;
         this.element.querySelector('ul')!.id = listId;
